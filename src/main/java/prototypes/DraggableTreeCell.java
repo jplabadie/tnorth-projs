eeef6eb8edd58fb7583f1591116e9532cd21d282
Luis Valenzuela <lav62@nau.edu>
2016-02-18 16:28:28 -0700

    Add CellFactory

---
+++ src/main/java/prototypes/DraggableTreeCell.java
@@ -0,0 +1,13 @@
+package prototypes;
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeCell;
+
+import java.io.File;
+
+/**
+ * Created by Nero on 2/18/2016.
+ */
+class DraggableTreeCell extends TreeCell<File> {
+
+}

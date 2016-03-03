ab679c92b7010dcbe771fa24ebf7b65b47aa1f38
knightpremier <labadie.jeanpaul@gmail.com>
2016-03-03 08:52:54 -0700

    Edit OutputParser to use new objects in xmlsources

--- src/main/java/prototypes/OutputParser.java
+++ src/main/java/prototypes/OutputParser.java
@@ -2,12 +2,10 @@ package prototypes;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import xmlsources.JobParameters;
+import xmlsources.NaspInputData;
+import xmlsources.ObjectFactory;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
@@ -26,28 +24,35 @@ public class OutputParser {
 
     public OutputParser(){}
 
-        public OutputParser jaxbXMLToObject(File input) {
-            try {
-                JAXBContext context = JAXBContext.newInstance(NASPJob.class);
-                Unmarshaller un = context.createUnmarshaller();
-                OutputParser job = (OutputParser) un.unmarshal(input);
-                return job;
-            } catch (JAXBException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-
-    /*
-    Writes the current XML DOM to the disk using the filename given
-    in the default output directory. This finished XML represents
-    the entirety of the NASP tool job request, and can be sent to
-    a remote service running NASP to begin a new job.
-    */
-    public void jaxbObjectToXML(JobParameters job, String name) {
+    /**\
+     *
+      * @param xml_path the absolute path to the xml file we will use to create Java objects
+     * @return a populated NaspInputData object with references to related classes
+     */
+    public NaspInputData jaxbXMLToObject(File xml_path) {
         try {
-            JAXBContext context = JAXBContext.newInstance(JobParameters.class);
+            JAXBContext context = JAXBContext.newInstance(ObjectFactory.class);
+            Unmarshaller un = context.createUnmarshaller();
+            NaspInputData naspData = ((JAXBElement<NaspInputData>) context.createUnmarshaller().unmarshal(xml_path)).getValue();
+            return naspData;
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     *  Writes the current XML DOM to the disk using the filename given
+     *  in the default output directory. This finished XML represents
+     *  the entirety of the NASP tool job request, and can be sent to
+     *  a remote service running NASP to begin a new job.
+     *
+     * @param input_for_conversion NaspInputData object which will be converted to XML for output to NASP
+     * @param output_path
+     */
+    public void jaxbObjectToXML(NaspInputData input_for_conversion, String output_path) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(NaspInputData.class);
             Marshaller m = context.createMarshaller();
             //for pretty-print XML in JAXB
             m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
@@ -57,7 +62,7 @@ public class OutputParser {
             // m.marshal(emp, System.out);
 
             // Write to File
-            m.marshal(job, new File(name));
+            m.marshal(input_for_conversion, new File(output_path));
         } catch (JAXBException e) {
             e.printStackTrace();
         }

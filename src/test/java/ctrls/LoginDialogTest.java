6f74a300a2efa285ea2cced45832a0f25b5c7981
knightpremier <labadie.jeanpaul@gmail.com>
2016-04-03 21:37:28 -0700

    Add tests for login dialog

---
+++ src/test/java/ctrls/LoginDialogTest.java
@@ -0,0 +1,28 @@
+package ctrls;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Jean-Paul Labadie
+ */
+public class LoginDialogTest extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("NASPGuiMainLayout.fxml"));
+        primaryStage.setTitle("NASP GUI Prototype");
+        primaryStage.setScene(new Scene(root, 1024, 800));
+        primaryStage.show();
+
+        LoginDialog temp = new LoginDialog();
+        temp.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

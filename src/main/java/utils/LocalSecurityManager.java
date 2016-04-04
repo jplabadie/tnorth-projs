dccdc84467637b587fd2a061a8ba3c698b5dc24b
knightpremier <labadie.jeanpaul@gmail.com>
2016-04-03 21:32:39 -0700

    Create utility class for managing security

--- src/main/java/utils/LocalSecurityManager.java
+++ src/main/java/utils/LocalSecurityManager.java
@@ -1,33 +1,28 @@
 package utils;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
 import javax.xml.bind.DatatypeConverter;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
 
+/**
+ * Provides an optional utility for securing user login to the GUI.
+ * Also provides optional 2-way encryption of locally saved user authentication for remote services.
+ * No guarantee is given with these optional functions, and security remains the responsibility of the user.
+ */
 public class LocalSecurityManager
 {
-    @SuppressWarnings("serial")
-    static public class InvalidHashException extends Exception {
-        public InvalidHashException(String message) {
-            super(message);
-        }
-        public InvalidHashException(String message, Throwable source) {
-            super(message, source);
-        }
-    }
-
-    @SuppressWarnings("serial")
-    static public class CannotPerformOperationException extends Exception {
-        public CannotPerformOperationException(String message) {
-            super(message);
-        }
-        public CannotPerformOperationException(String message, Throwable source) {
-            super(message, source);
-        }
-    }
 
     public static final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA1";
 
@@ -44,12 +39,143 @@ public class LocalSecurityManager
     public static final int SALT_INDEX = 3;
     public static final int PBKDF2_INDEX = 4;
 
+    private static KeyStore initKeyStore(String keystoreFileOrPath, String password) throws Exception {
+
+        File file = new File(keystoreFileOrPath);
+        final KeyStore keyStore = KeyStore.getInstance("JCEKS");
+
+        if (file.exists()) {
+            // .keystore file already exists => load it
+            keyStore.load(new FileInputStream(file), password.toCharArray());
+        } else {
+            // .keystore file not created yet => create it
+            keyStore.load(null, null);
+            keyStore.store(new FileOutputStream(keystoreFileOrPath), password.toCharArray());
+        }
+
+        return keyStore;
+    }
+
+    /**
+     *
+     */
+    public static void saveSecretKey(){
+
+        final String keyStoreFile = "output/javacirecep.keystore";
+        KeyStore keyStore = null;
+        try {
+            keyStore = initKeyStore(keyStoreFile, "javaci123");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Base64.Encoder encoder = Base64.getEncoder();
+
+        // generate a secret key for AES encryption
+        SecretKey secretKey = null;
+        try {
+            secretKey = KeyGenerator.getInstance("AES").generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Failed to generate key with specified algorithm.");
+        }
+        System.out.println("Stored Key: " + encoder.encodeToString(secretKey.getEncoded()));
+
+        // store the secret key
+        KeyStore.SecretKeyEntry keyStoreEntry = new KeyStore.SecretKeyEntry(secretKey);
+        KeyStore.PasswordProtection keyPassword = new KeyStore.PasswordProtection("pw-secret".toCharArray());
+        try {
+            keyStore.setEntry("mySecretKey", keyStoreEntry, keyPassword);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+        try {
+            keyStore.store(new FileOutputStream(keyStoreFile), "javaci123".toCharArray());
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @param password
+     */
+    public static void loadHash(char[] password){
+
+        final String keyStoreFile = "output/javacirecep.keystore";
+        KeyStore keyStore = null;
+        try {
+            keyStore = initKeyStore(keyStoreFile, "javaci123");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // retrieve the stored key back
+        KeyStore.Entry entry = null;
+
+        try {
+            KeyStore.ProtectionParameter protParameter =
+                    new KeyStore.PasswordProtection(password);
+            entry = keyStore.getEntry("mySecretKey", protParameter);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableEntryException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+        SecretKey keyFound = ((KeyStore.SecretKeyEntry) entry).getSecretKey();
+        System.out.println("Found Key: " + (keyFound));
+    }
+
+    /**
+     *
+     */
+    @SuppressWarnings("serial")
+    static public class InvalidHashException extends Exception {
+        public InvalidHashException(String message) {
+            super(message);
+        }
+        public InvalidHashException(String message, Throwable source) {
+            super(message, source);
+        }
+    }
+
+    /**
+     *
+     */
+    @SuppressWarnings("serial")
+    static public class CannotPerformOperationException extends Exception {
+        public CannotPerformOperationException(String message) {
+            super(message);
+        }
+        public CannotPerformOperationException(String message, Throwable source) {
+            super(message, source);
+        }
+    }
+
+
+    /**
+     *
+     * @param password
+     * @return
+     * @throws CannotPerformOperationException
+     */
     public static String createHash(String password)
             throws CannotPerformOperationException
     {
         return createHash(password.toCharArray());
     }
 
+    /**
+     *
+     * @param password
+     * @return
+     * @throws CannotPerformOperationException
+     */
     public static String createHash(char[] password)
             throws CannotPerformOperationException
     {
@@ -59,7 +185,7 @@ public class LocalSecurityManager
         random.nextBytes(salt);
 
         // Hash the password
-        byte[] hash = pbkdf2(password, salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
+        byte[] hash = getSecretKey(password, salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
         int hashSize = hash.length;
 
         // format: algorithm:iterations:hashSize:salt:hash
@@ -145,7 +271,7 @@ public class LocalSecurityManager
             hash = fromBase64(params[PBKDF2_INDEX]);
         } catch (IllegalArgumentException ex) {
             throw new InvalidHashException(
-                    "Base64 decoding of pbkdf2 output failed.",
+                    "Base64 decoding of getSecretKey output failed.",
                     ex
             );
         }
@@ -169,7 +295,7 @@ public class LocalSecurityManager
 
         // Compute the hash of the provided password, using the same salt,
         // iteration count, and hash length
-        byte[] testHash = pbkdf2(password, salt, iterations, hash.length);
+        byte[] testHash = getSecretKey(password, salt, iterations, hash.length);
         // Compare the hashes in constant time. The password is correct if
         // both hashes match.
         return slowEquals(hash, testHash);
@@ -198,7 +324,7 @@ public class LocalSecurityManager
      * @return
      * @throws CannotPerformOperationException
      */
-    private static byte[] pbkdf2(char[] password, byte[] salt, int iterations, int bytes)
+    private static byte[] getSecretKey(char[] password, byte[] salt, int iterations, int bytes)
             throws CannotPerformOperationException
     {
         try {
@@ -220,8 +346,8 @@ public class LocalSecurityManager
 
     /**
      *
-     * @param hex
-     * @return
+     * @param hex input base64 encoded string
+     * @return output binary byte array
      * @throws IllegalArgumentException
      */
     private static byte[] fromBase64(String hex)
@@ -239,5 +365,25 @@ public class LocalSecurityManager
     {
         return DatatypeConverter.printBase64Binary(array);
     }
+
+
+    /**
+     *
+     * @param property
+     * @param password
+     * @param salt
+     * @return
+     * @throws GeneralSecurityException
+     * @throws IOException
+     */
+    private static String decrypt(String property, char[] password, byte[] salt) throws GeneralSecurityException, IOException {
+
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(PBKDF2_ALGORITHM);
+        SecretKey key = keyFactory.generateSecret(new PBEKeySpec(password));
+        Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDES");
+        pbeCipher.init(Cipher.DECRYPT_MODE, key, new PBEParameterSpec(salt, 20));
+        return new String(pbeCipher.doFinal(fromBase64(property)), "UTF-8");
+    }
+
 
 }

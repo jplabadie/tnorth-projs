1f2a2b491899875b557aacf65a590e444e81273a
knightpremier <labadie.jeanpaul@gmail.com>
2016-04-10 22:42:49 -0700

    Add class to potentially manage users/configs/settings

---
+++ src/main/java/utils/UserConfigurationManager.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * Managers user details and default settings
+ *
+ * @author Jean-Paul Labadie
+ */
+public class UserConfigurationManager {
+
+    private static String username = "";
+
+    public static String getUsername() {
+        return username;
+    }
+}

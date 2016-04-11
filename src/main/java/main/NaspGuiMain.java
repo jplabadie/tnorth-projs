62a7a4e886e839d314912913098c1eb4e6d995b4
knightpremier <labadie.jeanpaul@gmail.com>
2016-04-10 23:32:08 -0700

    Edit stylesheet, add stylesheet to scene

--- src/main/java/main/NaspGuiMain.java
+++ src/main/java/main/NaspGuiMain.java
@@ -12,7 +12,9 @@ public class NaspGuiMain extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main/NASPGuiMainLayout.fxml"));
         primaryStage.setTitle("NASP GUI Prototype");
-        primaryStage.setScene(new Scene(root, 1024, 800));
+        Scene scene = new Scene(root, 1024, 800);
+        scene.getStylesheets().add(getClass().getResource("default.css").toExternalForm());
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 

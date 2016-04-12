665d7b796ca9d4ac3663f86a0c4662fd81f1848b
knightpremier <labadie.jeanpaul@gmail.com>
2016-04-12 09:07:01 -0700

    Add basic job dispatching functionality using NetworkManager

--- src/main/java/utils/JobDispatchManager.java
+++ src/main/java/utils/JobDispatchManager.java
@@ -1,6 +1,9 @@
 package utils;
 
+import com.jcraft.jsch.JSchException;
+
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Runs a finalized Job XML on NASP.
@@ -10,14 +13,47 @@ import java.io.File;
  */
 public class JobDispatchManager {
 
-    private static LogManager log;
+    private static LogManager log = LogManager.getInstance();
+    private static JobDispatchManager instance = new JobDispatchManager();
+    private NetworkManager net_mgr = new NetworkManager();
 
-    public JobDispatchManager(LogManager lm){
-        log = lm;
+    private JobDispatchManager(){
+
     }
 
-    public void startNewRemoteJob(File nasp_xml, String usrname, String password, String url, int port){
+    /**
+     *
+     * @return JobDispatchManager Singleton
+     */
+    public static JobDispatchManager getInstance(){
+        return instance;
+    }
+
+    /**
+     *
+     * @param nasp_xml the prepared NASP XML
+     * @param usrname the username for the remote server
+     * @param password the password for the remote server with given username
+     * @param url the url of the remote server as a String
+     * @param port the port of the remote server
+     * @param remote_path the directory in which to save the uploaded xml on the remote server
+     */
+    public void startNewRemoteJob(File nasp_xml, String usrname, String password, String url, int port, String remote_path){
 
         log.info("Remote Job Requested:");
+        DispatchConfiguration dc = new DispatchConfiguration(usrname,url, LogManager.getTimestamp(),port,remote_path);
+        log.logJob(dc);
+
+        net_mgr.initSession(usrname,password,url,port);
+        try {
+            net_mgr.openSession();
+        } catch (JSchException | IOException e) {
+            log.error("Failed to open Session.");
+            e.printStackTrace();
+        }
+
+        net_mgr.upload(nasp_xml,remote_path);
+
+        net_mgr.runNaspJob(remote_path);
     }
 }

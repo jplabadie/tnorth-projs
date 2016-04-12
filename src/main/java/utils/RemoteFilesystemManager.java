b71fa2638c26ae1a9e958c2fcff15e377c1a5b36
knightpremier <labadie.jeanpaul@gmail.com>
2016-04-12 02:17:12 -0700

    Edit for reliability and support of remote path browsing

--- src/main/java/utils/RemoteFilesystemManager.java
+++ src/main/java/utils/RemoteFilesystemManager.java
@@ -1,8 +1,11 @@
 package utils;
 
 import com.pastdev.jsch.DefaultSessionFactory;
+import com.pastdev.jsch.IOUtils;
+import com.pastdev.jsch.nio.file.UnixSshFileSystemProvider;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.*;
@@ -20,6 +23,7 @@ import java.util.Map;
 public class RemoteFileSystemManager {
 
     private FileSystem sshfs;
+    private LogManager log;
 
     /**
      *
@@ -33,19 +37,39 @@ public class RemoteFileSystemManager {
     public void init(String usrname, String pass, String hosturl, int port) throws URISyntaxException, IOException {
         String str = "";
 
-        DefaultSessionFactory dsf = new DefaultSessionFactory(usrname,hosturl,port);
+        DefaultSessionFactory dsf = new DefaultSessionFactory(usrname, hosturl, port);
         dsf.setPassword(pass);
 
         Map<String, Object> environment = new HashMap<String, Object>();
-        environment.put( "defaultSessionFactory", dsf );
+        environment.put("defaultSessionFactory", dsf);
 
         //Start the session URI looking in the user's home directory
-        URI uri = new URI( "ssh.unix://" + dsf.getUsername() + "@" + dsf.getHostname() + ":"
-                + dsf.getPort() + "/home/" +dsf.getUsername() );
+        URI uri = new URI("ssh.unix://" + dsf.getUsername() + "@" + dsf.getHostname() + ":"
+                + dsf.getPort() + "/home/" + dsf.getUsername());
 
-        sshfs = FileSystems.newFileSystem( uri, environment );
+        // Try to start the FileSystem with the default providers
+        try {
+            sshfs = FileSystems.newFileSystem(uri, environment);
+            Path path = sshfs.getPath("/"); // refers to /home/joe/afile
+            try (InputStream inputStream = path.getFileSystem().provider().newInputStream(path)) {
+                String fileContents = IOUtils.copyToString(inputStream);
+            }
+        }
+        // Try to start the FileSystem explicitly using the jsch-nio UnixSSH Provider
+        catch(ProviderNotFoundException e){
+
+            try{
+                UnixSshFileSystemProvider ussh = new UnixSshFileSystemProvider();
+                System.out.println(ussh.getScheme());
+                sshfs = FileSystems.newFileSystem(uri,environment,ussh.getClass().getClassLoader());
+            }
+            catch (Exception f)
+            {
+                System.out.println("Fuck!");
+                f.printStackTrace();
+            }
+        }
     }
-
     /**
      *
      * @param requested_path the path on the remote machine to explore
@@ -62,5 +86,33 @@ public class RemoteFileSystemManager {
         try (DirectoryStream<Path> ds = Files.newDirectoryStream(path)) {
            return ds;
         }
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Path getRootAsPath() throws IOException {
+        if(sshfs == null || !sshfs.isOpen()){
+            throw new ClosedDirectoryStreamException();
+        }
+
+        Path root_path= null;
+        for(Path path :sshfs.getRootDirectories()) // refers to an absolute remote path
+        {
+            root_path = path;
+            System.out.println(root_path.toString());
+            System.out.println(root_path.getFileSystem().getSeparator());
+            System.out.println(root_path.toUri().toString());
+        }
+        return root_path;
+    }
+
+    /**
+     *
+     * @return the state of the remote FileSystem, true for connected, false for disconnected
+     */
+    public boolean isConnected() {
+        return  sshfs.isOpen();
     }
 }

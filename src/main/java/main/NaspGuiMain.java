a99a4ad7e7002f84227228b1d530523f885abe6b
knightpremier <labadie.jeanpaul@gmail.com>
2016-04-12 02:14:39 -0700

    Edit minor aspects of the launcher for exploratory purposes

--- src/main/java/main/NaspGuiMain.java
+++ src/main/java/main/NaspGuiMain.java
@@ -1,6 +1,8 @@
 package main;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -18,14 +20,29 @@ public class NaspGuiMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main/NASPGuiMainLayout.fxml"));
-        primaryStage.setTitle("NASP GUI Prototype");
+        primaryStage.setTitle("NASP GUI Beta");
         Scene scene = new Scene(root, 1024, 800);
         scene.getStylesheets().add(getClass().getClassLoader().getResource("css/default.css").toExternalForm());
+
+
+        scene.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+                System.out.println("Width: " + newSceneWidth);
+            }
+        });
+        scene.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
+                System.out.println("Height: " + newSceneHeight);
+            }
+        });
+
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
-        launch(args);
+
+        Application.launch(args);
     }
 }

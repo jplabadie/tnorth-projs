8bd36734f0279de2fbd6864b5453375283f8c4ad
knightpremier <labadie.jeanpaul@gmail.com>
2016-04-25 01:11:37 -0700

    Add test class for JobManager

---
+++ src/test/java/utils/JobManagerTest.java
@@ -0,0 +1,16 @@
+package utils;
+
+import org.junit.Test;
+
+/**
+ * @author Jean-Paul Labadie
+ */
+public class JobManagerTest {
+
+    @Test
+    public void testStartNewRemoteJob() throws Exception {
+
+
+
+    }
+}

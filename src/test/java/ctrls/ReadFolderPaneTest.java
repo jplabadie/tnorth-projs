3084015dd8b8f0293248d583e69243abe6670dec
knightpremier <labadie.jeanpaul@gmail.com>
2016-04-28 02:40:36 -0700

    Add new class, defines Pane programmatically

--- src/test/java/ctrls/ReadFolderPaneTest.java
+++ src/test/java/ctrls/ReadFolderPaneTest.java
@@ -1,14 +1,19 @@
 package ctrls;
 
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * @author Jean-Paul Labadie
  * @date 8/11/2015
  */
-public class ReadFolderPaneTest {
+public class ReadFolderPaneTest extends Application {
 
     @Test
     public void testSetReadPairs() throws Exception {
@@ -27,6 +32,34 @@ public class ReadFolderPaneTest {
 
     @Test
     public void testGetNode() throws Exception {
+        Application.launch();
+    }
 
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Circle circ = new Circle(4, 4, 3);
+        Group root = new Group(circ);
+
+        ReadFolderPane test = new ReadFolderPane();
+        root.getChildren().add(test.getReadPane());
+
+        Scene scene = new Scene(root, 800, 600);
+
+        scene.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+                System.out.println("Width: " + newSceneWidth);
+            }
+        });
+        scene.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
+                System.out.println("Height: " + newSceneHeight);
+            }
+        });
+
+        Stage stage = new Stage();
+
+        stage.setScene(scene);
+        stage.show();
     }
 }

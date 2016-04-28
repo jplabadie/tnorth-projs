ddc8860ce0b9945a8a1abd0a178d0a60a47db913
knightpremier <labadie.jeanpaul@gmail.com>
2016-04-28 02:40:03 -0700

    Add tests for ReadFolderPane

---
+++ src/test/java/ctrls/ReadFolderPaneTest.java
@@ -0,0 +1,32 @@
+package ctrls;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Jean-Paul Labadie
+ * @date 8/11/2015
+ */
+public class ReadFolderPaneTest {
+
+    @Test
+    public void testSetReadPairs() throws Exception {
+
+    }
+
+    @Test
+    public void testGetReadPairs() throws Exception {
+
+    }
+
+    @Test
+    public void testGetReadPane() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNode() throws Exception {
+
+    }
+}

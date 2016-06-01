0436d5411e04b79e7d1e8ebc5d1f633a9d28eb5e
JP Labadie <jlabadie@tgen.org>
2016-06-01 11:44:18 -0700

    Create CLI using JOptSimple to parse args

--- snpdist_java/src/main/java/SNPDistributionMain.java
+++ snpdist_java/src/main/java/SNPDistributionMain.java
@@ -1,8 +1,46 @@
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.util.DateConverter;
+
+import java.io.File;
+import java.io.IOException;
+
+import static java.io.File.pathSeparatorChar;
+import static java.util.Arrays.*;
+
+import static joptsimple.util.DateConverter.*;
+
 /**
  * Project tnorth-projs.
  * Created by jlabadie on 6/1/16.
  *
- * @Author jlabadie
+ * @author jlabadie
  */
 public class SNPDistributionMain {
+
+    public static void main(String[] args) throws IOException {
+        OptionParser parser = new OptionParser() {
+            {
+                accepts( "c" ).withRequiredArg().ofType( Integer.class )
+                        .describedAs( "complete" ).defaultsTo( 1 );
+                accepts( "i" ).withOptionalArg().ofType( Double.class )
+                        .describedAs( "individual" );
+                accepts( "a", "agg" ).withRequiredArg().required()
+                        .withValuesConvertedBy( datePattern( "MM/dd/yy" ) );
+                accepts( "output-file" ).withOptionalArg().ofType( File.class )
+                        .describedAs( "file" );
+                acceptsAll( asList( "h", "?" ), "help" ).forHelp();
+                acceptsAll( asList( "cp", "classpath" ) ).withRequiredArg()
+                        .describedAs( "path1" + pathSeparatorChar + "path2:..." )
+                        .ofType( File.class )
+                        .withValuesSeparatedBy( pathSeparatorChar );
+            }
+        };
+        parser.posixlyCorrect(true);
+        parser.printHelpOn(System.out);
+
+        OptionSet opts = parser.parse(args);
+
+        opts.
+    }
 }

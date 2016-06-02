3ae1e7d83e378c80be42c1eedce0ae64b2e84963
JP Labadie <jlabadie@tgen.org>
2016-06-02 15:10:54 -0700

    Delete old CLI main, refactor module

--- snpdistjava/src/main/java/SNPDistributionMain.java
+++ snpdistjava/src/main/java/SNPDistributionMain.java
@@ -38,50 +38,50 @@ public class SNPDistributionMain {
         }
         catch (Exception e){
             System.out.println("Failed: Expected a path to a best-snps.tsv in the first argument field. " +
-                    "\nUse the -help option for syntax guidance.");
+                    "Use the -help option for syntax guidance.");
         }
 
         OptionParser parser = new OptionParser() {
             {
                 accepts( "c" ).withRequiredArg().ofType( String.class )
-                        .describedAs( "complete - gives an aggregate distribution and distributions for each sample.\n" +
+                        .describedAs( "complete - gives an aggregate distribution and distributions for each sample." +
                                 "Must be followed by a path representing where to write the output as a CSV." );
 
                 accepts( "i" ).withRequiredArg().ofType( String.class )
-                        .describedAs( "individual - gives distributions for a single sample.\n" +
+                        .describedAs( "individual - gives distributions for a single sample." +
                                 "Must be followed by a path representing where to write the output as a CSV." );
 
                 accepts( "im" ).withRequiredArg().ofType( String.class )
-                        .describedAs( "individual multiple - gives the distribution for multiple specific samples.\n" +
-                                "Must be followed by a list of samples, separated by commas, or as a range using a colon\n" +
+                        .describedAs( "individual multiple - gives the distribution for multiple specific samples." +
+                                "Must be followed by a list of samples, separated by commas, or as a range using a colon" +
                                 "Must then be followed by a path representing where to write the output as a CSV." );
 
-                accepts( "n" , "by number - alerts the program to expect samples to referenced by\n" +
+                accepts( "n" , "by number - alerts the program to expect samples to referenced by" +
                         "their position in the list of samples, rather than their name." ).availableIf( "im" );
 
                 accepts( "a" ).withRequiredArg().ofType( String.class )
-                        .describedAs( "aggregate - gives the distribution across all samples summed\n" +
+                        .describedAs( "aggregate - gives the distribution across all samples summed" +
                                 "Must be followed by a path representing where to write the output as a CSV." );
 
                 accepts( "w" ).withRequiredArg().ofType( Integer.class )
-                    .describedAs( "window size - specify the window size for surveying SNP distribution.\n" +
+                    .describedAs( "window size - specify the window size for surveying SNP distribution." +
                             "Must be followed by a sane non-zero value. Defaults to 1000." );
 
                 accepts( "s" ).withRequiredArg().ofType( Integer.class )
-                        .describedAs( "step size - specify the step size for the survey window.\n" +
-                                "If step size = window size, the window does not have a sliding behavior.\n" +
+                        .describedAs( "step size - specify the step size for the survey window." +
+                                "If step size = window size, the window does not have a sliding behavior." +
                                 "Must be followed by a sane non-zero value. Defaults to 1000." );
 
                 accepts( "ow" ).withRequiredArg().ofType(String.class)
-                        .describedAs("over-write - attempts to overwrite a file at the given output path.\n" +
+                        .describedAs("over-write - attempts to overwrite a file at the given output path." +
                                 "Must be followed by the string 'true' as a confirmation." );
 
                 acceptsAll( asList( "h", "?", "help", "info" ), "help" ).forHelp();
 
-                acceptsAll( asList( "cp", "classpath" ) ).withRequiredArg()
+                /*acceptsAll( asList( "cp", "classpath" ) ).withRequiredArg()
                         .describedAs( "path1" + pathSeparatorChar + "path2:..." )
                         .ofType( File.class )
-                        .withValuesSeparatedBy( pathSeparatorChar );
+                        .withValuesSeparatedBy( pathSeparatorChar );*/
             }
         };
         parser.posixlyCorrect(true);
